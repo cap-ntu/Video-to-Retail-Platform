c96cce91b1caf25768197fcb2163f1a34ae6c20b
Huaizheng <HUAIZHEN001@e.ntu.edu.sg>
2020-03-05 14:24:16 +0800

    Update server/model_server/utils.py
    Co-Authored-By: Yuanming Li <yli056@e.ntu.edu.sg>

--- server/model_server/utils.py
+++ server/model_server/utils.py
@@ -29,6 +29,6 @@ def timeit(method):
             name = kw.get('log_name', method.__name__.upper())
             kw['log_time'][name] = int((te - ts) * 1000)
         else:
-            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+            print(f'{method.__name__!r}  {(te - ts) * 1000:2.2f} ms')
         return result
     return timed
