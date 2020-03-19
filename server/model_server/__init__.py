64996354842e10baa199f6ac048906ad636fbfd0
YuanmingLeee <YLI056@e.ntu.edu.sg>
2020-03-19 12:08:03 +0800

    [Experiment 2.2] Update gpu util related graphs

--- server/model_server/__init__.py
+++ server/model_server/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 current_dir = Path(__file__).absolute().parent
 
 # Add lib to PYTHONPATH
-root_dir = current_dir.parents[2]
+root_dir = current_dir.parents[1]
 third_dir = root_dir / 'third'
 
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
